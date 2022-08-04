ddb32d5e30f798f7834e1ca92d4055be7e91bd03
Schecher_1 <Schecher_1@web.de>
2022-08-04 20:33:10 +0200

    add the start screen.

--- Core/ConsoleUI.cs
+++ Core/ConsoleUI.cs
@@ -8,11 +8,28 @@
         {
             while (isRunning)
             {
-
+                WriteStartScrren();
+                
             }
         }
-        
-        
+
+        private static void WriteStartScrren()
+        {
+            cls();
+            Console.WriteLine($"srm+      =>   Start Revice Messages");
+            Console.WriteLine($"srm-      =>   Stop Revice Messages");
+            Console.WriteLine($"confs     =>   Show the current Configs");
+            Console.WriteLine($"rl        =>   Reload Config File");
+            Console.WriteLine($"stat      =>   Show the status");
+            Console.WriteLine($"logs      =>   Show the Log-Terminal");
+            Console.WriteLine($"errlogs   =>   Shows all Error logs");
+            Console.WriteLine($"warlogs   =>   Shows all Warning logs");
+            Console.WriteLine($"scslogs   =>   Shows all Successful logs");
+            Console.WriteLine($"q         =>   Stop the Bot");
+        }
+
+        private static void cls()
+            => Console.Clear();
 
     }
 }
