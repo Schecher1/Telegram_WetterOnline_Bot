2b7829dba27baf84eda2643de9518899110e8ada
Schecher_1 <Schecher_1@web.de>
2022-08-04 20:58:00 +0200

    add log model

---
+++ Model/LogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Telegram_WetterOnline_Bot.Model
+{
+    public class LogModel
+    {
+        public Logger.LogLevel LogLevel { get; set; }
+        public string Log { get; set; }
+    }
+}
