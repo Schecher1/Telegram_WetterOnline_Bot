99b91eafd3b04b77d8b59a00588a3a7053494b8b
Schecher_1 <Schecher_1@web.de>
2023-12-12 19:05:24 +0100

    remove config system (using docker vars)

--- Classes/StartUp.cs
+++ Classes/StartUp.cs
@@ -10,14 +10,28 @@
             if (!Config.InitConfigFile())
                 return;
 
+
+            //get env vars 
+            string telegramToken = Environment.GetEnvironmentVariable("TELEGRAM_API_TOKEN");
+            string telegramOwnerName = Environment.GetEnvironmentVariable("TELEGRAMBOT_OWNER_NAME");
+            int[] telegramIDWhitelist = Array.ConvertAll(Environment.GetEnvironmentVariable("TELEGRAM_ID_WHITELIST").Split(';'), int.Parse);
+
+            //check if env vars are empty
+            if (string.IsNullOrEmpty(telegramToken) || string.IsNullOrEmpty(telegramOwnerName) || telegramIDWhitelist is null)
+            {
+                Console.WriteLine("One or more Environment Variables are empty!");
+                Console.WriteLine("Telegram_API_Token: " + telegramToken);
+                Console.WriteLine("Telegrambot_Owner_Name: " + telegramOwnerName);
+                Console.WriteLine("Telegrambot_ID_Whitelist: " + telegramIDWhitelist);
+                return;
+            }
+
             TelegramBot bot = new TelegramBot();
 
             bot.SetAPIKey(EnvironmentVariable.TELEGRAM_API_TOKEN);
 
             bot.Init();
             TelegramBot.StartRM();
-
-            //ConsoleUI.StartConsole();
         }
     }
 }
