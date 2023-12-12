9fe074be463c03535676e4a0c2d536f880920ba4
Schecher_1 <Schecher_1@web.de>
2023-12-12 19:18:14 +0100

    remove env var bugs

--- Classes/StartUp.cs
+++ Classes/StartUp.cs
@@ -4,31 +4,48 @@
     {
         public StartUp()
             => Start();
+
         private void Start()
         {
             //get env vars 
-            string telegramToken = Environment.GetEnvironmentVariable("TELEGRAM_API_TOKEN");
-            string telegramOwnerName = Environment.GetEnvironmentVariable("TELEGRAMBOT_OWNER_NAME");
-            int[] telegramIDWhitelist = Array.ConvertAll(Environment.GetEnvironmentVariable("TELEGRAM_ID_WHITELIST").Split(';'), int.Parse);
+            try
+            {
+                string telegramToken = Environment.GetEnvironmentVariable("TELEGRAM_API_TOKEN");
+                string telegramOwnerName = Environment.GetEnvironmentVariable("TELEGRAMBOT_OWNER_NAME");
+                int[] telegramIDWhitelist = Array.ConvertAll(Environment.GetEnvironmentVariable("TELEGRAM_ID_WHITELIST").Split(';'), int.Parse);
 
-            //check if env vars are empty
-            if (string.IsNullOrEmpty(telegramToken) || string.IsNullOrEmpty(telegramOwnerName) || telegramIDWhitelist is null)
+                //check if env vars are empty
+                if (string.IsNullOrEmpty(telegramToken) || string.IsNullOrEmpty(telegramOwnerName) || telegramIDWhitelist is null)
+                {
+                    Console.WriteLine("One or more Environment Variables are empty!");
+                    Console.WriteLine("Telegram_API_Token: " + telegramToken);
+                    Console.WriteLine("Telegrambot_Owner_Name: " + telegramOwnerName);
+                    Console.WriteLine("Telegrambot_ID_Whitelist: " + telegramIDWhitelist);
+                    return;
+                }
+
+                //set env data
+                EnvironmentVariable.TELEGRAM_API_TOKEN = telegramToken;
+                EnvironmentVariable.TELEGRAMBOT_OWNER_NAME = telegramOwnerName;
+                EnvironmentVariable.TELEGRAM_ID_WHITELIST = telegramIDWhitelist;
+
+                //bot init
+                TelegramBot bot = new TelegramBot();
+                bot.SetAPIKey(EnvironmentVariable.TELEGRAM_API_TOKEN);
+                bot.Init();
+                bot.StartRM();
+
+                //let the docker container run 4 ever
+                Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine("One or more Environment Variables are empty!");
-                Console.WriteLine("Telegram_API_Token: " + telegramToken);
-                Console.WriteLine("Telegrambot_Owner_Name: " + telegramOwnerName);
-                Console.WriteLine("Telegrambot_ID_Whitelist: " + telegramIDWhitelist);
-                return;
+                Console.WriteLine("Telegram_API_Token: " + Environment.GetEnvironmentVariable("TELEGRAM_API_TOKEN"));
+                Console.WriteLine("Telegrambot_Owner_Name: " + Environment.GetEnvironmentVariable("TELEGRAMBOT_OWNER_NAME"));
+                Console.WriteLine("Telegrambot_ID_Whitelist: " + Environment.GetEnvironmentVariable("TELEGRAM_ID_WHITELIST"));
+                Console.WriteLine(ex.Message);
             }
-
-            TelegramBot bot = new TelegramBot();
-
-            bot.SetAPIKey(telegramToken);
-            bot.Init();
-            bot.StartRM();
-
-            //let the docker container run 4 ever
-            Thread.Sleep(Timeout.Infinite);
         }
     }
 }
