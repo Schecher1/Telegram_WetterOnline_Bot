9f1fe62127e8ee55ff2136af113b1b7c954aff1a
Schecher_1 <Schecher_1@web.de>
2024-01-09 21:00:26 +0100

    add data handler boiler

---
+++ Handler/DataHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram_WetterOnline_Bot.Handler
+{
+    public class DataHandler
+    {
+        private const string JSON_PATH = @".\Data\";
+        private const string JSON_FILE = "metaData.json";
+
+        
+    }
+}
