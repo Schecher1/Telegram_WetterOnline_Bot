a7d59e3016e30dd060dfd25842b2b51dc19f29e5
Schecher_1 <Schecher_1@web.de>
2024-01-14 16:53:26 +0100

    FIX: from 12:5 =>12:05

--- Core/TelegramBot.cs
+++ Core/TelegramBot.cs
@@ -330,7 +330,8 @@ namespace Telegram_WetterOnline_Bot.Core
             {
                 var button = new InlineKeyboardButton
                 {
-                    Text = $"{job.Time.Hours}:{job.Time.Minutes} Uhr - {job.Location.Substring(0, 1).ToUpper() + job.Location.Substring(1)}",
+                    //shows the time and the location of the timer with 2 zeros in front of the hour if the hour is smaller than 10
+                    Text = $"{(job.Time.Hours < 10 ? "0" : "")}{job.Time.Hours}:{(job.Time.Minutes < 10 ? "0" : "")}{job.Time.Minutes}=={job.Location}",
                     CallbackData = $"deleteTimer_{job.Id}"
                 };
 
